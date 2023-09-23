0ae72c7b83bbf4d8df09d03746425dc76c32d86d
Matheus Fernandes <matheus.almeida@grendene.com.br>
2023-09-23 14:24:56 -0300

    feat(Aluno): Ganhando tempo com o uso de Scaffolding

--- AppMvcFuncional/Data/ApplicationDbContext.cs
+++ AppMvcFuncional/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using AppMvcFuncional.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace AppMvcFuncional.Data
@@ -9,5 +10,6 @@ namespace AppMvcFuncional.Data
             : base(options)
         {
         }
+        public DbSet<Aluno> Aluno { get; set; }
     }
 }
